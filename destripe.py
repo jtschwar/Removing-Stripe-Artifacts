cc6dbd69e5bc2af6622e898d81aed0686c054a9a
jtschw@umich.edu <jtschw@umich.edu>
2019-09-11 15:58:09 -0400

    Change transperency of missing wedge.

--- destripe.py
+++ destripe.py
@@ -44,7 +44,8 @@ class destripe:
 		#Artifact Removal Loop
 		for i in range(self.Niter):
 
-			print('Iteration No.: ' + str(i+1) +'/'+str(self.Niter))
+			if (i+1) % 25 == 0:
+				print('Iteration No.: ' + str(i+1) +'/'+str(self.Niter))
 
 			#FFT of Reconstructed Image.
 			FFT_recon = fftshift(self.fftw.fft(recon_init)) 	
@@ -136,6 +137,47 @@ class destripe:
 		mask = np.transpose(mask)
 		return mask
 
+	def create_mini_mask(self):
+
+		# Original data size
+		(nx, ny) = self.dataset.shape
+
+		(center_x, center_y) = (nx//2-1, ny//2-1)
+
+		#Area for missing wedge
+		(rnx, rny) = (nx//1.5, ny//1.5)
+
+		if self.theta > 90 or self.theta < -90:
+			print('Please keep theta between +/- 90 degrees.') 
+
+		# Convert missing wedge size and theta to radians.
+		rad_theta = -(self.theta+90)*(np.pi/180)
+		dtheta = self.wedgeSize*(np.pi/180)
+
+		#Create coordinate grid in polar 
+		x = np.arange(-rnx/2, rnx/2-1,dtype=np.float64)
+		y = np.arange(-rny/2, rny/2-1,dtype=np.float64)
+		[x,y] = np.meshgrid(x,y,indexing ='xy')
+		rr = (np.square(x) + np.square(y))
+		phi = np.arctan2(y,x) 
+		phi *= -1
+
+		#Create the Mask
+		mask = np.ones( (rny, rnx), dtype = np.int8 )
+		mask[np.where((phi >= (rad_theta-dtheta/2)) & (phi <= (rad_theta+dtheta/2)))]  = 0
+		mask[np.where((phi >= (np.pi+rad_theta-dtheta/2)) & (phi <= (np.pi+rad_theta+dtheta/2)))] = 0
+		mask[np.where((phi >= (-np.pi+rad_theta-dtheta/2)) & (phi <= (-np.pi+rad_theta+dtheta/2)))] = 0
+		mask[np.where(rr < np.square(self.kmin))] = 1 # Keep values below rmin.
+		mask = np.array(mask, dtype = bool)
+		mask = np.transpose(mask)
+
+		full_mask = np.ones((ny, nx), dtype = np.int8)
+		full_mask[center_x-rnx//2:center_x+rnx//2,center_y-rny//2:center_y+rny//2] = mask
+		full_mask = np.array(full_mask, dtype = bool)
+
+		return full_mask
+
+
 	def view_missing_wedge(self):
 		
 		self.fft_raw = np.log(np.abs(fftshift(self.fftw.fft(self.dataset))) + 1)
@@ -149,12 +191,12 @@ class destripe:
 		mask_edge[mask_edge > 0] = 1
 
 		fig, self.ax_list = plt.subplots(1,2, figsize=(14,6))
-		self.ax_list[0].imshow(self.dataset, cmap='bone')
+		self.ax_list[0].imshow(self.dataset, cmap='gray')
 		self.ax_list[0].set_title('Input Image')
 		self.ax_list[0].axis('off')
-		self.ax_list[1].imshow(self.fft_raw, cmap = 'bone')
+		self.ax_list[1].imshow(self.fft_raw, cmap = 'gray')
 		self.ax_list[1].set_title('FFT of Input Image')
-		self.ax_list[1].imshow(mask_edge, cmap='viridis_r')
+		self.ax_list[1].imshow(mask_edge, cmap='viridis_r', alpha=0.3)
 		self.ax_list[1].axis('off')
 		plt.tight_layout()
 		plt.draw()
@@ -169,8 +211,8 @@ class destripe:
 		mask_edge = np.ma.masked_where(mask_edge == 0, mask_edge)
 		mask_edge[mask_edge > 0] = 1
 
-		self.ax_list[1].imshow(self.fft_raw, cmap = 'bone')
-		self.ax_list[1].imshow(mask_edge, cmap='viridis_r')
+		self.ax_list[1].imshow(self.fft_raw, cmap = 'gray')
+		self.ax_list[1].imshow(mask_edge, cmap='viridis_r', alpha=0.3)
 
 	def edit_wedgeSize(self, new_wedgeSize):
 		self.wedgeSize = float(eval(new_wedgeSize))
